52dba2f0b71219c1da930018a78c469bc8a28f71
zynasiuk <54539663+zynasiuk@users.noreply.github.com>
2020-01-31 20:13:53 +0100

    step2: Roles added to php

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+// role admin or user; create user.php
 include_once'connectdb.php';
 session_start();
 if(isset($_POST['btn_login'])) {
@@ -10,10 +11,13 @@ if(isset($_POST['btn_login'])) {
     $select->execute();
 
     $row = $select->fetch(PDO::FETCH_ASSOC);
-   //     if($row['useremail']==$useremail AND $row['password']==$password AND !empty($useremail && $password)) { 
-    if($row['useremail']==$useremail AND $row['password']==$password) {
+
+    if($row['useremail']==$useremail AND $row['password']==$password AND $row['role']=="Admin") {
         echo $success="Login successful";
         header('refresh:1;dashboard.php');
+    } else if($row['useremail']==$useremail AND $row['password']==$password AND $row['role']=="User"){
+         echo "Login successful";
+        header('refresh:1;user.php');
     } else {
         echo 'Login failed';
     }
