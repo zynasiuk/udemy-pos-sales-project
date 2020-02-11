ab36f049eed10aef805cea47950f6e57c7e60ee8
zynasiuk <54539663+zynasiuk@users.noreply.github.com>
2020-02-11 15:12:05 +0100

    section 16: create order p.2

--- createorder.php
+++ createorder.php
@@ -1,6 +1,24 @@
 <?php
 include_once'connectdb.php';
 session_start();
+
+// krok1: pobierz wszystkie z tbl_product i tworz opcje do wybory tak dlugo jak sa wyniki zwrotu z db
+function fill_product($pdo) {
+    $output = '';
+    $select = $pdo->prepare("select * from tbl_product order by pname asc");
+    $select -> execute();
+    $result = $select -> fetchAll();
+    
+    foreach($result as $row) {
+        $output.= '<option value="'.$row["pid"].'">'.$row["pname"].'</option>';
+    }
+    return $output;
+}
+// krok2: uzyj select2 plugin aby moc rowniez wyszukiwac produkty
+
+// krok3: pobierz dane i wpisz je do innych pol w dziale z jQuery
+
+
 include_once'header.php';
 ?>
 
@@ -12,6 +30,7 @@ include_once'header.php';
     <!-- Content Header (Page header) -->
     <section class="content-header">
         <h1>
+
             Create Order
             <small></small>
         </h1>
@@ -74,22 +93,25 @@ include_once'header.php';
                     <!-- table -->
 
                     <div class="col-md-12">
-                        <table id="producttable" class="table table-striped">
-                            <thead>
-                                <tr>
-                                    <th>#</th>
-                                    <th>Search Product</th>
-                                    <th>Stock</th>
-                                    <th>Price</th>
-                                    <th>Enter Quantity</th>
-                                    <th>Total</th>
-                                    <th> <button type="button" name="add" class="btn btn-success btn-sm btnadd"><span class="glyphicon glyphicon-plus"></span></button></th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                            </tbody>
-                        </table>
-
+                        <div style="overflow-x:auto;">
+                            <table id="producttable" class="table table-striped">
+                                <thead>
+                                    <tr>
+                                        <th>#</th>
+                                        <th>Search Product</th>
+                                        <th>Stock</th>
+                                        <th>Price</th>
+                                        <th>Enter Quantity</th>
+                                        <th>Total</th>
+                                        <th>
+                                            <center><button type="button" name="add" class="btn btn-success btn-sm btnadd"><span class="glyphicon glyphicon-plus"></span></button></center>
+                                        </th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
 
                 </div>
@@ -192,7 +214,7 @@ include_once'header.php';
                     <input type="submit" name="btnsaveorder" value="Save Order" class="btn btn-info">
                 </div>
 
-<hr>
+                <hr>
 
 
             </form>
@@ -214,19 +236,73 @@ include_once'header.php';
         checkboxClass: 'icheckbox_flat-green',
         radioClass: 'iradio_flat-green'
     });
-    
-    $(document).ready(function(){
+
+
+    // add products to your order; FETCH DATA function ABOVE! - here insert fill_product($pdo) php code in select
+    $(document).ready(function() {
         $(document).on('click', '.btnadd', function() {
-            var html='';
-            html+='<tr>';
-            html+='<td><input type="text" class="form-control pname" name="productname[]" readonly></td>';
-            html+='<td><input type="text" class="form-control productid" name="productid[]" required></td>';
-            html+='<td><input type="text" class="form-control stock" name="stock[]" required></td>';
-            html+='<td><input type="text" class="form-control price" name="price[]" required></td>';
-            html+='<td><input type="text" class="form-control qty" name="qty[]" required></td>';
-            html+='<td><input type="text" class="form-control total" name="total[]" required></td>';
+            var html = '';
+            html += '<tr>';
+            html += '<td><input type="hidden" class="form-control pname" name="productname[]" readonly></td>';
+
+            html += '<td><select class="form-control productid" name="productid[]" style="width: 200px;"><option value="">Select Option</option><?php echo fill_product($pdo);?></select></td>';
+            html += '<td><input type="text" class="form-control stock" name="stock[]" readonly></td>';
+            html += '<td><input type="text" class="form-control price" name="price[]" readonly></td>';
+            html += '<td><input type="number" min="1" class="form-control qty" name="qty[]"></td>';
+            html += '<td><input type="text" class="form-control total" name="total[]" readonly></td>';
+
+            html += '<td><center><button type="button" name="remove" class="btn btn-danger btn-sm btnremove"><span class="glyphicon glyphicon-remove"></span></button></center></td></tr>';
+
             $('#producttable').append(html);
+
+            //Initialize Select2 Elements
+            $('.productid').select2();
+
+            $(".productid").on('change', function(e) {
+                var productid = this.value;
+                var tr = $(this).parent().parent();
+                $.ajax({
+
+                    //w getproduct.php polacz sie z db i pobierz dane z tabeli produktow na podstawie id
+                    url: "getproduct.php",
+                    method: "get",
+                    data: {
+                        id: productid
+                    },
+                    success: function(data) {
+
+                        tr.find(".stock").val(data["pstock"]);
+                        tr.find(".price").val(data["saleprice"]);
+                        tr.find(".qty").val(1);
+
+                        tr.find(".total").val(tr.find(".qty").val() * tr.find(".price").val());
+
+
+                    }
+                })
+            })
+        });
+
+        // remove products from your order
+        $(document).on('click', '.btnremove', function() {
+            $(this).closest('tr').remove();
         })
+
+        // oblicz i wyswietl cene za ilosc produktow
+
+        $('#producttable').delegate(".qty", "keyup change", function() {
+            var quantity = $(this);
+            var tr = $(this).parent().parent();
+            if ((quantity.val() - 0) > (tr.find(".stock").val() - 0)) {
+                swal("WARNING", "Not availiby in this number of quantity", "warning");
+
+                quantity.val(1);
+                tr.find(".total").val(quantity.val() * tr.find(".price").val());
+            } else {
+                tr.find(".total").val(quantity.val() * tr.find(".price").val());
+            }
+        })
+
     });
 
 </script>
--- productlist.php
+++ productlist.php
@@ -10,24 +10,24 @@ include_once'header.php';
 
 
 
-  <!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper">
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
     <!-- Content Header (Page header) -->
     <section class="content-header">
-      <h1>
-        Product List
-        <small></small>
-      </h1>
-      <ol class="breadcrumb">
-        <li><a href="#"><i class="fa fa-dashboard"></i> Level</a></li>
-        <li class="active">Here</li>
-      </ol>
+        <h1>
+            Product List
+            <small></small>
+        </h1>
+        <ol class="breadcrumb">
+            <li><a href="#"><i class="fa fa-dashboard"></i> Level</a></li>
+            <li class="active">Here</li>
+        </ol>
     </section>
 
     <!-- Main content -->
     <section class="content container-fluid">
 
-      <!--------------------------
+        <!--------------------------
         | Your Page Content Here |
         -------------------------->
         <div class="box box-warning">
@@ -35,24 +35,25 @@ include_once'header.php';
                 <h3 class="box-title">Product List</h3>
             </div>
             <div class="box-body">
-                      <table id="producttable" class="table table-striped">
-                            <thead>
-                                <tr>
-                                    <th>#</th>
-                                    <th>Product Name</th>
-                                    <th>Category</th>
-                                    <th>Purchase Price</th>
-                                    <th>Sale Price</th>
-                                    <th>Stock</th>
-                                    <th>Description</th>
-                                    <th>Image</th>
-                                    <th>View</th>
-                                    <th>Edit</th>
-                                    <th>Delete</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <?php
+                <div style="overflow-x:auto;">
+                    <table id="producttable" class="table table-striped">
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Product Name</th>
+                                <th>Category</th>
+                                <th>Purchase Price</th>
+                                <th>Sale Price</th>
+                                <th>Stock</th>
+                                <th>Description</th>
+                                <th>Image</th>
+                                <th>View</th>
+                                <th>Edit</th>
+                                <th>Delete</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
         $select=$pdo->prepare("select * from tbl_product order by pid desc");
         $select->execute();
         
@@ -81,70 +82,74 @@ include_once'header.php';
         }        
         
         ?>
-                            </tbody>
-                        </table>
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     </section>
     <!-- /.content -->
-  </div>
-  <!-- /.content-wrapper -->
+</div>
+<!-- /.content-wrapper -->
 
 
 <script>
     //zmniejszajaca sie kolejnosc
-$(document).ready(function () {
-    $('#producttable').DataTable({
-        "order":[[0,"desc"]]
+    $(document).ready(function() {
+        $('#producttable').DataTable({
+            "order": [
+                [0, "desc"]
+            ]
+        });
     });
-} );
-//tooltip - bootstrapowy hint
-$(document).ready(function () {
-    $('[data-toggle="tooltip"]').tooltip();
-} );
+    //tooltip - bootstrapowy hint
+    $(document).ready(function() {
+        $('[data-toggle="tooltip"]').tooltip();
+    });
 
-// ajax
-$(document).ready(function(){
-    $('.btndelete').click(function(){
-       // alert("test");
-        let tdh = $(this);
-        let id = $(this).attr("id");
-        // alert(id);
-        
-        
-        swal({
-  title: "Are you sure?",
-  text: "Once deleted, you will not be able to recover this product!",
-  icon: "warning",
-  buttons: true,
-  dangerMode: true,
-})
-.then((willDelete) => {
-  if (willDelete) {
-      
-          $.ajax({
-        url:'productdelete.php',
-        type: 'post',
-        data: {
-            pidd: id
-        },
-        success:function(data) {
-            tdh.parents('tr').hide();
-        }
+    // ajax
+    $(document).ready(function() {
+        $('.btndelete').click(function() {
+            // alert("test");
+            let tdh = $(this);
+            let id = $(this).attr("id");
+            // alert(id);
+
+
+            swal({
+                    title: "Are you sure?",
+                    text: "Once deleted, you will not be able to recover this product!",
+                    icon: "warning",
+                    buttons: true,
+                    dangerMode: true,
+                })
+                .then((willDelete) => {
+                    if (willDelete) {
+
+                        $.ajax({
+                            url: 'productdelete.php',
+                            type: 'post',
+                            data: {
+                                pidd: id
+                            },
+                            success: function(data) {
+                                tdh.parents('tr').hide();
+                            }
+                        });
+
+
+                        swal("Your product has been deleted!", {
+                            icon: "success",
+                        });
+                    } else {
+                        swal("Your product is safe!");
+                    }
+                });
+
+
+        });
     });
-      
-      
-    swal("Your product has been deleted!", {
-      icon: "success",
-    });
-  } else {
-    swal("Your product is safe!");
-  }
-});
-        
-    
-    });
-});
+
 </script>
 
 <?php
